95057e1808d19202abb7b3881093b6e1cc2ede81
hirano-vm4 <hirano.vm4@gmail.com>
2024-02-01 20:13:12 +0900

    デザインの設定

---
+++ src/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Icon from "../src/images/1.png";
+
+const Logo = () => {
+  return <img src={Icon} alt="ThinkLog" className="SidebarLogo"></img>;
+};
+
+export default Logo;
